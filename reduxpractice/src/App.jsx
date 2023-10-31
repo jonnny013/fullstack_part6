c042ce640af116d966dbd594562a577dde2505ff
Jon Love <124639155+jonnny013@users.noreply.github.com>
2023-10-31 20:42:45 +0800

    move notereducer to its own file and make a test for it

--- reduxpractice/src/App.jsx
+++ reduxpractice/src/App.jsx
@@ -1,15 +1,34 @@
 import { useState } from 'react'
 import './App.css'
+import { createStore } from 'redux'
 
-function App() {
-  const [count, setCount] = useState(0)
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+    case 'ZERO':
+      return 0
+    default: // if none of the above matches, code comes here
+      return state
+  }
+}
 
+const store = createStore(counterReducer)
+
+
+
+const App = () => {
   return (
-    <>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-    </>
+    <div>
+      <div>
+        {store.getState()}
+      </div>
+      <button onClick={e => store.dispatch({type: 'INCREMENT'})}>plus</button>
+      <button onClick={e => store.dispatch({type: 'DECREMENT'})}>minus</button>
+      <button onClick={e => store.dispatch({type: 'ZERO'})}>zero</button>
+    </div>
   )
 }
 
