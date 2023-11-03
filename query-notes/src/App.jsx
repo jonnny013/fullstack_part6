ed16354bb35f8b3f3324248d6fee1f88f6e20ac3
Jon Love <124639155+jonnny013@users.noreply.github.com>
2023-11-03 10:47:12 +0800

    Add react query to renewed query notes

---
+++ query-notes/src/App.jsx
@@ -0,0 +1,70 @@
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+import { getNotes, createNote, updateNote } from './requests'
+
+const App = () => {
+  
+  const queryClient = useQueryClient()
+  const newNoteMutation = useMutation({ 
+    mutationFn: createNote,
+    onSuccess: (newNote) => {
+      const notes = queryClient.getQueryData(['notes'])
+      queryClient.setQueryData(['notes'], notes.concat(newNote))
+    }
+  })
+
+  const addNote = async (event) => {
+    event.preventDefault()
+    const content = event.target.note.value
+    event.target.note.value = ''
+    newNoteMutation.mutate({ content, important: true})
+  }
+
+  const result = useQuery({
+    queryKey: ['notes'],
+    queryFn: getNotes,
+    refetchOnWindowFocus: false
+  })
+  console.log(JSON.parse(JSON.stringify(result)))
+
+  const updateNoteMutation = useMutation({
+    mutationFn: updateNote,
+    onSuccess: (updatedNote) => {
+      const notes = queryClient.getQueryData(['notes'])
+      console.log(updatedNote)
+      queryClient.setQueryData(['notes'], notes.map((note) => {
+        if (note.id === updatedNote.id) {
+          return {...note, important: updatedNote.important}
+        }
+        return note
+      }))
+    }
+  })
+
+  const notes = result.data
+
+  const toggleImportance = (note) => {
+    updateNoteMutation.mutate({...note, important: !note.important})
+  }
+
+  if ( result.isLoading ) {
+    return <div>loading data...</div>
+  }
+
+  return(
+    <div>
+      <h2>Notes app</h2>
+      <form onSubmit={addNote}>
+        <input name="note" />
+        <button type="submit">add</button>
+      </form>
+      {notes.map(note =>
+        <li key={note.id} onClick={() => toggleImportance(note)}>
+          {note.content} 
+          <strong> {note.important ? 'important' : ''}</strong>
+        </li>
+      )}
+    </div>
+  )
+}
+
+export default App
